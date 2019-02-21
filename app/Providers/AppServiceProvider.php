192448f52edb9ebcc0f1d9c44b329708a55054e1
trusV <trus.dev@gmail.com>
2019-02-21 23:31:18 +0200

    repositories and interfaces was bound

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Repositories\ImagesRepository\Contracts\ImageRepositoryInterface;
+use App\Repositories\ImagesRepository\ImageRepository;
+use App\Repositories\SuperheroRepository\Contracts\SuperheroRepositoryInterface;
+use App\Repositories\SuperheroRepository\SuperheroRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +27,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(ImageRepositoryInterface::class, ImageRepository::class);
+        $this->app->bind(SuperheroRepositoryInterface::class, SuperheroRepository::class);
     }
 }
